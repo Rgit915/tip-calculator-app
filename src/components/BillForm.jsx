dac0202141d1e965f1d1141695ba6a6d50e6e3e8
Rgit915 <rora.goitom@gmail.com>
2025-05-17 08:37:39 -0400

    Make inputs controlled, add tip button handlers

--- src/components/BillForm.jsx
+++ src/components/BillForm.jsx
@@ -1,17 +1,29 @@
 import personIcon from "../assets/images/icon-person.svg";
 
-const BillForm = () => {
+const BillForm = ({
+  bill,
+  setBill,
+  tipPercent,
+  setTipPercent,
+  people,
+  setPeople,
+}) => {
+  const tipOptions = ["5", "10", "15", "25", "50"];
   return (
-    <section className="bill-container w-full">
+    <section className="bill-container w-full lg:flex-1">
       <form>
-        <label htmlFor="bill" className="block mb-2">Bill</label>
-        <div className="bill flex justify-between items-center w-full rounded-md bg-neutral-grey-50 text-neutral-grey-800 px-4 py-2">
+        <label htmlFor="bill" className="block mb-2">
+          Bill
+        </label>
+        <div className="bill flex justify-between items-center w-full rounded-md bg-neutral-grey-50 text-neutral-grey-800 px-4 py-2 hover:border-2 hover:border-primary-green-400 cursor-pointer">
           <label htmlFor="amount">$</label>
           <input
             id="bill"
             name="bill"
             type="text"
             placeholder="0"
+            value={bill}
+            onChange={(e) => setBill(e.target.value)}
             className="ml-4 w-full bg-transparent text-right outline-none"
           />
         </div>
@@ -23,19 +35,22 @@ const BillForm = () => {
           >
             Select Tip %
           </label>
-          <div className="grid grid-cols-2 gap-4">
-            {["5%", "10%", "15%", "25%", "50%"].map((tip) => (
+          <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+            {tipOptions.map((tip) => (
               <button
                 key={tip}
                 type="button"
-                className="bg-neutral-grey-800 text-white text-2xl font-bold py-2 rounded-md hover:bg-neutral-grey-700 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                onClick={() => setTipPercent(tip)}
+                className="bg-neutral-grey-800 text-white text-2xl font-bold py-2 rounded-md hover:bg-neutral-green-200 hover:text-neutral-green-900 cursor-pointer"
               >
-                {tip}
+                {tip}%
               </button>
             ))}
             <button
               type="button"
-              className="bg-neutral-grey-50 text-neutral-grey-500 text-2xl font-bold py-2 rounded-md hover:bg-neutral-grey-300 focus:outline-none focus:ring-2 focus:ring-primary-500"
+              value={!tipOptions.includes(tipPercent) ? tipPercent : ""}
+              onChange={(e) => setTipPercent(e.target.value)}
+              className="bg-neutral-grey-50 text-neutral-grey-500 text-2xl font-bold py-2 rounded-md hover:bg-neutral-grey-300 cursor-pointer"
             >
               Custom
             </button>
@@ -56,6 +71,8 @@ const BillForm = () => {
               name="people"
               type="text"
               placeholder="0"
+              value={people}
+              onChange={(e) => setPeople(e.target.value)}
               className="ml-4 w-full bg-transparent text-right outline-none placeholder:text-neutral-grey-400"
             />
           </div>
