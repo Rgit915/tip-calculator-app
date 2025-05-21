3ce74274c62e4bbdd89503e39ca2a49ef14411c7
Rgit915 <rora.goitom@gmail.com>
2025-05-20 23:16:07 -0400

    Add missing <h1> for accessibility compliance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import logo from "../assets/images/logo.svg";
 const Header = () => (
   <header>
     <div className="flex items-center justify-center py-16">
+      <h1 className="sr-only">Tip Calculator</h1>
       <img src={logo} alt="logo image" />
     </div>
   </header>
